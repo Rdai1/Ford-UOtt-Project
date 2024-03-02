c8022492293dc3837b5c6c20c533ba890e39103a
Mahad T <mahad.toukaleh56@gmail.com>
2024-03-02 15:23:13 -0500

    .js to .tsx

---
+++ index.tsx
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('SERVER running on PORT ${PORT}'));
